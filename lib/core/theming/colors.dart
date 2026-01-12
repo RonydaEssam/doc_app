da3763bd039a4eb48b39ca9b6c8626f9a1705426
RonydaEssam <ronyda988@gmail.com>
2026-01-12 19:18:57 +0200

    theming colors

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color mainBlue = Color(0xFF247CFF);
+  static const Color green = Color(0xFF22C55E);
+  static const Color red = Color(0xFFFF4C5E);
+  static const Color grey = Color(0xFF757575);
+  static const Color black = Color(0xFF242424);
+  static const Color yellow = Color(0xFFFFD600);
+}
