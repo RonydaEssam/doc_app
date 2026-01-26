28bede04b45ab94c6f124d5e9b54f33eb78ed423
Ronyda Essam <ronyda988@gmail.com>
2026-01-26 14:59:07 +0200

    add initial home screen and home screen route

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,8 @@
 import 'package:doc_app/core/dependency_injection/dependency_injection.dart';
 import 'package:doc_app/core/routing/routes.dart';
-import 'package:doc_app/features/login/data/logic/cubit/login_cubit.dart';
-import 'package:doc_app/features/login/login_screen.dart';
+import 'package:doc_app/features/home/ui/screens/home_screen.dart';
+import 'package:doc_app/features/login/logic/cubit/login_cubit.dart';
+import 'package:doc_app/features/login/ui/login_screen.dart';
 import 'package:doc_app/features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,6 +17,13 @@ class AppRouter {
           builder: (_) => BlocProvider(
             create: (context) => getIt<LoginCubit>(),
             child: const LoginScreen(),
+          ),
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const HomeScreen(),
           ),
         );
       default:
